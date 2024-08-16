81f927bcfe68fb17c69db08a36ad8ee7028d066c
mdaffailhami <mdaffailhami@gmail.com>
2024-08-16 13:18:06 +0800

    Add "add-location" route

--- web/app/Config/Routes.php
+++ web/app/Config/Routes.php
@@ -7,3 +7,4 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 $routes->get('/add-project', 'AddProject::index');
+$routes->get('/add-location', 'AddLocation::index');
