ac110b43c2bf8c8e4eab982eac17ecdb6fb203a3
mdaffailhami <mdaffailhami@gmail.com>
2024-08-18 13:03:14 +0800

    Rename home to beranda

--- web/app/Config/Routes.php
+++ web/app/Config/Routes.php
@@ -5,6 +5,6 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Beranda::index');
 $routes->get('/tambah-proyek', 'TambahProyek::index');
 $routes->get('/tambah-lokasi', 'TambahLokasi::index');
--- web/app/Views/beranda.php
+++ web/app/Views/beranda.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <link rel="stylesheet" href="/css/bootstrap.min.css" />
-    <script defer src="/js/home.js"></script>
+    <script defer src="/js/beranda.js"></script>
 
     <title>SEI - MSIB</title>
   </head>
